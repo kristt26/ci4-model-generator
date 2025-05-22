835a4818b0b926f2cae660d9b2774afb886d1dba
Ajenkris Yanto Kungkung <kristt26@gmail.com>
2025-05-23 06:16:49 +0900

    fix controole

--- src/ControllerGenerator.php
+++ src/ControllerGenerator.php
@@ -1,64 +1,57 @@
-<?php namespace Ci4ModelGenerator;
-
+<?php
 class ControllerGenerator
 {
     protected string $controllerName;
     protected string $modelName;
     protected string $namespace;
-    protected string $controllerFolder; // misal: Admin atau Api
+    protected string $controllerFolder;
 
-    public function __construct(string $controllerName, string $modelName, string $namespace = 'App\Controllers', string $controllerFolder = '')
+    public function __construct(string $controllerName, string $modelName, string $namespace, string $controllerFolder = '')
     {
         $this->controllerName = $controllerName;
         $this->modelName = $modelName;
         $this->namespace = $namespace;
-        $this->controllerFolder = trim($controllerFolder, '\\/');
+        $this->controllerFolder = $controllerFolder;
     }
 
     public function generate(): string
     {
-        $namespaceLine = $this->namespace;
-        if ($this->controllerFolder !== '') {
-            $namespaceLine .= '\\' . str_replace('/', '\\', $this->controllerFolder);
-        }
-
-        $modelFullName = "App\Models\\$this->modelName";
-
-        $controllerClassName = $this->controllerName;
+        $namespaceLine = "namespace {$this->namespace};";
 
         return <<<PHP
-<?php namespace $namespaceLine;
+<?php
+$namespaceLine
 
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 
-class $controllerClassName extends BaseController
+class {$this->controllerName} extends BaseController
 {
-    protected \$$this->modelName;
+    protected \${$this->controllerFolder ? strtolower($this->controllerFolder) . '_' : ''}{$this->modelName};
 
     public function __construct()
     {
-        \$this->{$this->modelName} = new $modelFullName();
+        \$this->{$this->controllerFolder ? strtolower($this->controllerFolder) . '_' : ''}{$this->modelName} = new \\App\\Models\\{$this->modelName}();
     }
 
     public function index(): string
     {
-        return view('{$this->controllerFolder}/" . strtolower($this->controllerName) . "');
+        return view('{$this->controllerFolder ? strtolower($this->controllerFolder) . '/' : ''}{$this->controllerName}');
     }
 
     public function store()
     {
-        return \$this->response->setJSON(\$this->{$this->modelName}->findAll());
+        return \$this->response->setJSON(\$this->{$this->controllerFolder ? strtolower($this->controllerFolder) . '_' : ''}{$this->modelName}->findAll());
     }
 
     public function add(): ResponseInterface
     {
         \$param = \$this->request->getJSON();
         try {
-            \$this->{$this->modelName}->insert(\$param);
-            \$param->id = \$this->{$this->modelName}->insertID();
+            \$this->{$this->controllerFolder ? strtolower($this->controllerFolder) . '_' : ''}{$this->modelName}->insert(\$param);
+            \$param->id = \$this->{$this->controllerFolder ? strtolower($this->controllerFolder) . '_' : ''}{$this->modelName}->insertID();
             return \$this->response->setJSON(\$param);
-        } catch (\\Throwable \$th) {
+        } catch (\Throwable \$th) {
             return \$this->response->setJSON([
                 'status' => 'error',
                 'message' => \$th->getMessage()
@@ -70,12 +63,12 @@ class $controllerClassName extends BaseController
     {
         \$param = \$this->request->getJSON();
         try {
-            \$this->{$this->modelName}->update(\$param->id, \$param);
+            \$this->{$this->controllerFolder ? strtolower($this->controllerFolder) . '_' : ''}{$this->modelName}->update(\$param->id, \$param);
             return \$this->response->setJSON([
                 'status' => 'success',
                 'message' => 'Data berhasil diubah'
             ]);
-        } catch (\\Throwable \$th) {
+        } catch (\Throwable \$th) {
             return \$this->response->setJSON([
                 'status' => 'error',
                 'message' => \$th->getMessage()
@@ -86,12 +79,12 @@ class $controllerClassName extends BaseController
     public function delete(\$id = null): ResponseInterface
     {
         try {
-            \$this->{$this->modelName}->delete(\$id);
+            \$this->{$this->controllerFolder ? strtolower($this->controllerFolder) . '_' : ''}{$this->modelName}->delete(\$id);
             return \$this->response->setJSON([
                 'status' => 'success',
                 'message' => 'Data berhasil dihapus'
             ]);
-        } catch (\\Throwable \$th) {
+        } catch (\Throwable \$th) {
             return \$this->response->setJSON([
                 'status' => 'error',
                 'message' => \$th->getMessage()
